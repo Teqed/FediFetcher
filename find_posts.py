2c2de476d6d2474464552bb3e73ebec754fb0e12
Timothy Quilling <teqed.arken@gmail.com>
2023-06-28 20:38:21 -0400

    undo: regex change

--- find_posts.py
+++ find_posts.py
@@ -534,11 +534,11 @@ def parse_pixelfed_profile_url(url):
 def parse_lemmy_url(url):
     """parse a Lemmy URL and return the server, and ID"""
     match = re.match(
-        r"https://(?P<server>[^/]+)/comment/(?P<toot_id>.*)", url
+        r"https://(?P<server>.*)/comment/(?P<toot_id>.*)", url
     )
     if match is None:
         match = re.match(
-            r"https://(?P<server>[^/]+)/post/(?P<toot_id>.*)", url
+            r"https://(?P<server>.*)/post/(?P<toot_id>.*)", url
         )
     if match is not None:
         return (match.group("server"), match.group("toot_id"))
@@ -546,7 +546,7 @@ def parse_lemmy_url(url):
 
 def parse_lemmy_profile_url(url):
     """parse a Lemmy Profile URL and return the server and username"""
-    match = re.match(r"https://(?P<server>[^/]+)/u/(?P<username>.*)", url)
+    match = re.match(r"https://(?P<server>.*)/u/(?P<username>.*)", url)
     if match is not None:
         return (match.group("server"), match.group("username"))
     return None
