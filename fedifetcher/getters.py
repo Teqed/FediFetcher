22a80a6c8fdaab20c01c849d0d3555b3b30d0dd1
Timothy Quilling <teqed.arken@gmail.com>
2023-07-04 16:58:40 -0400

    chore: fix try/except

--- fedifetcher/getters.py
+++ fedifetcher/getters.py
@@ -111,17 +111,18 @@ def get_user_posts_from_url(
     url = f"https://{parsed_url[0]}/api/v3/user?username={parsed_url[1]}&sort=New&limit=50"
     response = helpers.get(url)
 
-    if response.status_code == helpers.Response.OK:
-        comments = [post["post"] for post in response.json()["comments"]]
-        posts = [post["post"] for post in response.json()["posts"]]
-        all_posts = comments + posts
-        for post in all_posts:
-            post["url"] = post["ap_id"]
-        return all_posts
-
-    logging.error(
+    try:
+        if response.status_code == helpers.Response.OK:
+            comments = [post["post"] for post in response.json()["comments"]]
+            posts = [post["post"] for post in response.json()["posts"]]
+            all_posts = comments + posts
+            for post in all_posts:
+                post["url"] = post["ap_id"]
+            return all_posts
+    except:
+        logging.error(
 f"Error getting user posts for user {parsed_url[1]}: {response.text}")
-    return None
+        return None
 
 def get_community_posts_from_url(
         parsed_url : tuple[str, str]) -> list[dict[str, str]] | None:
