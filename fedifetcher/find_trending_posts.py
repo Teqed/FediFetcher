d8f237657f2be44af0ef3ae105d0ad1faf0a6e8a
Timothy Quilling <teqed@shatteredsky.net>
2023-07-20 18:45:01 -0400

    chore: don't add to dict

--- fedifetcher/find_trending_posts.py
+++ fedifetcher/find_trending_posts.py
@@ -264,7 +264,7 @@ async def aux_domain_fetch(external_tokens : dict[str, str],  # noqa: PLR0913
             if cached_post and cached_post["url"] in post_urls:
                 logging.debug(f"Found {cached_post['url']} in cache")
                 post_urls.remove(cached_post["url"]) # Check originality?
-                add_post_to_dict(cached_post, parsed_urls[0][0], trending_post_dict)
+                # add_post_to_dict(cached_post, parsed_urls[0][0], trending_post_dict)
         trending = await api_mastodon.get_trending_posts(
                         parsed_urls[0][0],
                         external_tokens.get(parsed_urls[0][0]), 40)
