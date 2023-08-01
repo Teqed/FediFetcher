46393d48aa6687f882190c83713ab9a80b95d424
Timothy Quilling <teqed@shatteredsky.net>
2023-08-01 07:25:33 -0400

    feat: firefish db

--- find_posts.py
+++ find_posts.py
@@ -139,7 +139,7 @@ below --lock-hours={helpers.arguments.lock_hours} provided.")
             host="dreamer", \
                 # TODO: Make this configurable
             port = 5432,
-            database="mastodon_production", \
+            database="firefish", \
                 # TODO: Make this configurable
             user="teq", \
                 # TODO: Make this configurable
